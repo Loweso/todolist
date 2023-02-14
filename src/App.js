c8e8a51ea072a09809ff227809d0d32b7c5d0018
Loweso <119489421+Loweso@users.noreply.github.com>
2023-02-14 12:09:17 +0800

    [UI][Maxell] added navbar

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
-import './App.css';
-import React, { useState } from 'react';
-import { Select } from './components/Button';
-import ToDoListModal from './components/Modal';
+import React from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  //naming convention on boolean variables
-  const [isOpen, setIsOpen] = useState(false);
-
-  //create a function to handle different actions such as clicking a button
-  const handleModalOpen = () => {
-    setIsOpen(true);
-  };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>
-          To-Do List
-          <button className="btn btn-primary" type="button" onClick={handleModalOpen}>
-            Add Task
-          </button>
-          <Select>
-            <option>All</option>
-            <option>Incomplete</option>
-            <option>Complete</option>
-          </Select>
-        </h2>
-        <div className="taskRoll">
-          <p>No activities yet!</p>
-        </div>
-        {isOpen && <ToDoListModal setIsOpen={setIsOpen} />}
-      </header>
+    <div style={{display:'flex'}}>
+      <Navbar/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
