1dc19dc88828fad02e3ed2d3441dfe95ab7a56b6
Loweso <119489421+Loweso@users.noreply.github.com>
2023-02-14 10:30:26 +0800

    [Adjustment][Louise] edited modal

--- src/ToDoListModal.js
+++ src/ToDoListModal.js
@@ -1,26 +1,10 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const ToDoListModal = ({show, setShow}) => {
-  console.log(show);
-  
-  const content = show && (
-    <div className="modal fade">
-      <div className="modal-dialog" role="document">
-        <div className="modal-content">
-          <button
-            type="button"
-            onClick={() => setShow(false)}
-          >
-            X
-          </button>
-          <div><h1>AMOGUS</h1></div>
-        </div>
-      </div>
-    </div>
-  );
-
-  return content
+const ToDoListModal = () => {
+  return (
+    <div className='modal-container-test'>Simple Modal</div>
+  )
 }
 
 export default ToDoListModal
