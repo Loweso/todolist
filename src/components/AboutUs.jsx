aaca692b556a169afb034029d986ad6a8f3eb9b0
Loweso <119489421+Loweso@users.noreply.github.com>
2024-02-06 14:37:09 +0800

    [Feature][Louise] Searching

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function AboutUs() {
+  return (
+    <div className="flex flex-col h-full">
+      <Navbar />
+      <div className="grid h-full justify-items-center pt-20 relative mt-20">
+        <p className="text-6xl text-center">About Us</p>
+        <p className="text-5xl text-center">
+          <br />
+          <br />
+          We are sadboisxzxczxczxx
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+  const navigate = useNavigate();
+  const handleTodoList = () => {
+    navigate("/");
+  };
+  const handleAboutUs = () => {
+    navigate("/aboutUs");
+  };
+
   return (
     <div className="bg-navbarcolor fixed flex h-28 w-full justify-between items-center z-10">
       <div className="ml-24 flex">
-        <p className="flex text-3xl">
-          <b>TODOLIST</b>
-        </p>
-        <button className="mx-5">
+        <button className="mx-5" onClick={handleTodoList}>
+          <p className="flex text-3xl text-opacity-50">
+            <b>TODOLIST</b>
+          </p>
+        </button>
+        <button className="mx-5" onClick={handleAboutUs}>
           <p className="flex text-2xl ml-24 text-opacity-50">
             <b>About Us</b>
           </p>
